bc728266f16eec2515959c692ca654930cf936d3
RenissonSilva <renissonx@gmail.com>
2019-11-21 20:37:30 -0300

    Adicionando registro e login na api

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,6 @@
 <?php
 
 Route::apiResource('personagems','api\PersonagemController');
+
+Route::post('/register', 'api\AuthController@register');
+Route::post('/login', 'api\AuthController@login');
